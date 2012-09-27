811ef7c0669e9461e1e599f10d6d10a4c9264497
Julian Doherty <julian@envato.com>
2012-09-27 16:22:58 +1000

    added timestamp module

---
+++ lib/fresh_redis/timestamp.rb
@@ -0,0 +1,17 @@
+class FreshRedis
+  module Timestamp
+    def normalize_key(key, t, granularity)
+      [key, normalize_time(t, granularity)].join(":")
+    end
+
+    def normalize_time(t, granularity)
+      t - (t % granularity)
+    end
+
+    def range_timestamps(t, freshness, granularity)
+      from = normalize_time(t - freshness, granularity)
+      to = normalize_time(t, granularity)
+      (from..to).step(granularity)
+    end
+  end    
+end
