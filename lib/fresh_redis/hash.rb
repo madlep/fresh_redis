042ed118ab9167a97f0467c94cbf2a2b1334726f
Julian Doherty (madlep) <madlep@madlep.com>
2012-10-14 22:32:04 +1100

    convert hash operations to use FreshRedis::Key

--- lib/fresh_redis/hash.rb
+++ lib/fresh_redis/hash.rb
@@ -1,48 +1,37 @@
 class FreshRedis
   module Hash
     def fhset(key, hash_key, value, options={})
-      options = default_options(options)
-      t           = options[:t]
-      freshness   = options[:freshness]
-      granularity = options[:granularity]
-
-      key = normalize_key(key, t, granularity)
+      key = Key.build(key, options)
       @redis.multi do
-        @redis.hset(key, hash_key, value)
-        @redis.expire(key, freshness)
+        @redis.hset(key.redis_key, hash_key, value)
+        @redis.expire(key.redis_key, key.freshness)
       end
     end
 
     def fhget(key, hash_key, options={})
-      options = default_options(options)
-      t           = options[:t]
-      freshness   = options[:freshness]
-      granularity = options[:granularity]
-
-      each_timestamped_key(key, t, freshness, granularity) do |timestamp_key|
-        @redis.hget(timestamp_key, hash_key)
-      end.compact
+      key = Key.build(key, options)
+      @redis.pipelined {
+        key.timestamp_buckets.each do |bucket_key|
+          @redis.hget(bucket_key, hash_key)
+        end
+      }.compact
     end
 
     def fhgetall(key, options={})
-      options = default_options(options)
-      t           = options[:t]
-      freshness   = options[:freshness]
-      granularity = options[:granularity]
-
-      each_timestamped_key(key, t, freshness, granularity) do |timestamp_key|
-        @redis.hgetall(timestamp_key)
-      end.reject { |hash| hash.count.zero? }
+      key = Key.build(key, options)
+      @redis.pipelined {
+        key.timestamp_buckets.each do |bucket_key|
+          @redis.hgetall(bucket_key)
+        end
+      }.reject { |hash| hash.count.zero? }
     end
 
     def fhdel(key, hash_key, options={})
-      options = default_options(options)
-      t           = options[:t]
-      freshness   = options[:freshness]
-      granularity = options[:granularity]
-
-      each_timestamped_key(key, t, freshness, granularity) do |timestamp_key|
-        @redis.hdel(timestamp_key, hash_key)
+      key = Key.build(key, options)
+      @redis.pipelined do
+        key.timestamp_buckets.each do |bucket_key|
+          @redis.hdel(bucket_key, hash_key)
+        end
       end
     end
   end
