18af01b3e5daacd525eb40cad60e606af7635908
Julian Doherty (madlep) <madlep@madlep.com>
2012-10-17 23:34:26 +1100

    update to using timecop rather than injecting time

--- spec/fresh_redis/hash_spec.rb
+++ spec/fresh_redis/hash_spec.rb
@@ -1,82 +1,85 @@
 require 'fresh_redis'
 require 'mock_redis'
+require 'timecop'
 
 describe FreshRedis do
   subject{ FreshRedis.new(mock_redis) }
-  let(:mock_redis) { MockRedis.new }
-  let(:now) { Time.new(2012, 9, 27, 15, 40, 56, "+10:00").to_i }
-  let(:normalized_now_minute) { Time.new(2012, 9, 27, 15, 40, 0, "+10:00").to_i }
-  let(:normalized_one_minute_ago) { Time.new(2012, 9, 27, 15, 39, 0, "+10:00").to_i }
-  let(:normalized_two_minutes_ago) { Time.new(2012, 9, 27, 15, 38, 0, "+10:00").to_i }
-  let(:normalized_old) { Time.new(2012, 9, 27, 14, 38, 0, "+10:00").to_i } 
+  let!(:mock_redis) { MockRedis.new }
+  let(:now) { Time.new(2012, 9, 27, 15, 40, 56, "+10:00") }
+  let(:normalized_now_minute) { Time.new(2012, 9, 27, 15, 40, 0, "+10:00") }
+  let(:normalized_one_minute_ago) { Time.new(2012, 9, 27, 15, 39, 0, "+10:00") }
+  let(:normalized_two_minutes_ago) { Time.new(2012, 9, 27, 15, 38, 0, "+10:00") }
+  let(:normalized_old) { Time.new(2012, 9, 27, 14, 38, 0, "+10:00") } 
 
   context "hash keys" do 
+    before(:each) { Timecop.travel(now) }
+    after(:each) { Timecop.return }
 
     describe "#fhset" do
       it "sets a value for a key in a hash for the normalized timestamp" do
-        subject.fhset "foo", "bar", "value", :t => now - 3
-        subject.fhset "foo", "bar", "newer_value", :t => now
-        subject.fhset "foo", "bar", "different_bucket", :t => now - 60 # different normalized key
+        Timecop.freeze(now - 3)   { subject.fhset "foo", "bar", "value" }
+        Timecop.freeze(now)       { subject.fhset "foo", "bar", "newer_value" }
+        Timecop.freeze(now - 60)  { subject.fhset "foo", "bar", "different_bucket" } # different normalized key
 
-        mock_redis.data["foo:#{normalized_now_minute}"].should == {"bar" => "newer_value"}
+        mock_redis.data["foo:#{normalized_now_minute.to_i}"].should == {"bar" => "newer_value"}
       end
 
       it "sets a nil value ok" do
-        subject.fhset "foo", "bar", nil, :t => now
+        Timecop.freeze(now) { subject.fhset "foo", "bar", nil }
 
-        mock_redis.data["foo:#{normalized_now_minute}"].should == {"bar" => "" }
+        mock_redis.data["foo:#{normalized_now_minute.to_i}"].should == {"bar" => "" }
       end
 
       it "sets the freshness as the expiry" do
         # relying on mock_redis's time handling here - which converts to/from using Time.now Possible flakey temporal breakage potential
-        subject.fhset "foo", "bar", "baz", :freshness => 3600, :t => now
+        subject.fhset "foo", "bar", "baz", :freshness => 3600
 
-        mock_redis.ttl("foo:#{normalized_now_minute}").should == 3600
+        mock_redis.ttl("foo:#{normalized_now_minute.to_i}").should == 3600
       end
     end
 
     describe "#fhdel" do
       it "removes the field from all timestamp buckets" do
-        subject.fhset "foo", "bar", "value", :t => now
-        subject.fhset "foo", "baz", "don't touch", :t => now
-        subject.fhset "foo", "bar", "different_bucket", :t => now - 60
-        subject.fhset "foo", "baz", "I shouldn't be returned", :t => now - 60
+        Timecop.travel(now)       { subject.fhset "foo", "bar", "value" }
+        Timecop.travel(now)       { subject.fhset "foo", "baz", "don't touch" }
+        Timecop.travel(now - 60)  { subject.fhset "foo", "bar", "different_bucket" }
+        Timecop.travel(now - 60)  { subject.fhset "foo", "baz", "I shouldn't be returned" }
 
-        subject.fhdel "foo", "bar", :t => now # Should only change the most recent bucket
+        Timecop.travel(now)       { subject.fhdel "foo", "bar" } # Should only change the most recent bucket
 
-        mock_redis.data["foo:#{normalized_now_minute}"].should == {"baz" => "don't touch"}
-        mock_redis.data["foo:#{normalized_one_minute_ago}"].should == {"baz" => "I shouldn't be returned"}
+        mock_redis.data["foo:#{normalized_now_minute.to_i}"].should == {"baz" => "don't touch"}
+        mock_redis.data["foo:#{normalized_one_minute_ago.to_i}"].should == {"baz" => "I shouldn't be returned"}
       end
     end
 
     describe "#fhget" do
       it "gets the most recent value for the field across timestamped buckets" do
-        mock_redis.hset "foo:#{normalized_now_minute}", "notbar", "francis"
-        mock_redis.hset "foo:#{normalized_one_minute_ago}", "bar", "bill"
-        mock_redis.hset "foo:#{normalized_two_minutes_ago}", "bar", "louis"
+        mock_redis.hset "foo:#{normalized_now_minute.to_i}", "notbar", "francis"
+        mock_redis.hset "foo:#{normalized_one_minute_ago.to_i}", "bar", "bill"
+        mock_redis.hset "foo:#{normalized_two_minutes_ago.to_i}", "bar", "louis"
 
-        subject.fhget("foo", "bar", :t => now).should == "bill"
+        subject.fhget("foo", "bar").should == "bill"
       end
 
       it "returns nil if value is not found" do
-        subject.fhget("foo", "bar", :t => now).should be_nil
+        subject.fhget("foo", "bar").should be_nil
       end
 
       it "returns nil if the value is in a bucket that has expired" do
         # this should be handled by redis expiry anyway, but verify code is behaving as expected and not querying more data than needed
-        mock_redis.hset "foo:#{normalized_old}", "bar", "louis"
-        subject.fhget("foo", "bar", :t => now).should be_nil
+        mock_redis.hset "foo:#{normalized_old.to_i}", "bar", "louis"
+        subject.fhget("foo", "bar").should be_nil
       end
     end
 
     describe "#fhgetall" do
       it "merges the values for all keys across timestamp buckets" do
-        mock_redis.hset "foo:#{normalized_now_minute}", "bar", "francis"
-        mock_redis.hset "foo:#{normalized_one_minute_ago}", "baz", "zoey"
-        mock_redis.hset "foo:#{normalized_one_minute_ago}", "bar", "I shouldn't be returned"
-        mock_redis.hset "foo:#{normalized_two_minutes_ago}", "boz", "louis"
+        mock_redis.hset "foo:#{normalized_now_minute.to_i}", "bar", "francis"
+        mock_redis.hset "foo:#{normalized_one_minute_ago.to_i}", "baz", "zoey"
+        mock_redis.hset "foo:#{normalized_one_minute_ago.to_i}", "bar", "I shouldn't be returned"
+        mock_redis.hset "foo:#{normalized_two_minutes_ago.to_i}", "boz", "louis"
 
-        subject.fhgetall("foo", :t => now).should == { "bar" => "francis", "baz" => "zoey", "boz" => "louis" }
+        subject.fhgetall("foo").should == { "bar" => "francis", "baz" => "zoey", "boz" => "louis" }
       end
     end
   end
--- spec/fresh_redis/string_spec.rb
+++ spec/fresh_redis/string_spec.rb
@@ -1,43 +1,47 @@
 require 'fresh_redis'
 require 'mock_redis'
+require 'timecop'
 
 describe FreshRedis do
   subject{ FreshRedis.new(mock_redis) }
   let(:mock_redis) { MockRedis.new }
-  let(:now) { Time.new(2012, 9, 27, 15, 40, 56, "+10:00").to_i }
-  let(:normalized_now_minute) { Time.new(2012, 9, 27, 15, 40, 0, "+10:00").to_i }
+  let(:now) { Time.new(2012, 9, 27, 15, 40, 56, "+10:00") }
+  let(:normalized_now_minute) { Time.new(2012, 9, 27, 15, 40, 0, "+10:00") }
+
+  before(:each) { Timecop.travel(now) }
+  after(:each) { Timecop.return }
 
   context "string keys" do
     describe "#fincr" do
       it "should increment the key for the normalized timestamp" do
-        subject.fincr "foo", :t => now
-        subject.fincr "foo", :t => now + 3
-        subject.fincr "foo", :t => now + 60 # different normalized key
-        mock_redis.data["foo:#{normalized_now_minute}"].to_i.should == 2
+        Timecop.freeze(now)       { subject.fincr "foo" }
+        Timecop.freeze(now + 3)   { subject.fincr "foo" }
+        Timecop.freeze(now + 60)  { subject.fincr "foo" } # different normalized key
+        mock_redis.data["foo:#{normalized_now_minute.to_i}"].to_i.should == 2
       end
 
       it "should set the freshness as the expiry" do
         # relying on mock_redis's time handling here - which converts to/from using Time.now Possible flakey temporal breakage potential
-        subject.fincr "foo", :freshness => 3600, :t => now
-        mock_redis.ttl("foo:#{normalized_now_minute}").should == 3600
+        subject.fincr "foo", :freshness => 3600
+        mock_redis.ttl("foo:#{normalized_now_minute.to_i}").should == 3600
       end
     end
 
     describe "#fsum" do
       subject{ FreshRedis.new(mock_redis, :granularity => 10, :freshness => 60) }
       it "should add the values of keys for specified freshness and granularity" do
-        subject.fincr "foo", :t => now - 60 - 10
-        subject.fincr "foo", :t => now - 60 + 1
-        subject.fincr "foo", :t => now - 60 + 2
-        subject.fincr "foo", :t => now - 60 + 3
-        subject.fincr "foo", :t => now - 60 + 5
-        subject.fincr "foo", :t => now - 60 + 8
-        subject.fincr "foo", :t => now - 60 + 13
-        subject.fincr "foo", :t => now - 60 + 21
-        subject.fincr "foo", :t => now - 60 + 34
-        subject.fincr "foo", :t => now - 60 + 55
+        Timecop.freeze(now - 60 - 10) { subject.fincr "foo" }
+        Timecop.freeze(now - 60 + 1)  { subject.fincr "foo" }
+        Timecop.freeze(now - 60 + 2)  { subject.fincr "foo" }
+        Timecop.freeze(now - 60 + 3)  { subject.fincr "foo" }
+        Timecop.freeze(now - 60 + 5)  { subject.fincr "foo" }
+        Timecop.freeze(now - 60 + 8)  { subject.fincr "foo" }
+        Timecop.freeze(now - 60 + 13) { subject.fincr "foo" }
+        Timecop.freeze(now - 60 + 21) { subject.fincr "foo" }
+        Timecop.freeze(now - 60 + 34) { subject.fincr "foo" }
+        Timecop.freeze(now - 60 + 55) { subject.fincr "foo" }
 
-        subject.fsum("foo", :t => now).should ==  9
+        subject.fsum("foo").should ==  9
       end
     end
 
